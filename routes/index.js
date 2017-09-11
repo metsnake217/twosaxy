c4168624f288c872c4d4e349c7b386d889891115
N'Deye Gnagna Marianne Konate <marianne@function1.com>
2017-09-10 21:53:35 -0400

    Pushing prelim files for 2saxy

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var dates = require('../config/staticvariables');
+var moment = require('moment-timezone');
+var jstz = require('jstimezonedetect');
+
+var express = require('express');
+var util = require('util');
+var router = express.Router();
+
+module.exports = function(router) {
+
+	router.get('/', function(req, res) {
+		res.redirect('/home');
+	});
+
+	router.get('/home', function(req, res) {
+		res.render('home', {
+			title : 'Home',
+			menu : 'home'
+		});
+	});
+
+	router.get('/about', isLoggedIn, function(req, res) {
+		res.render('about', {
+			title : 'About Us',
+			menu : 'about'
+		});
+	});
+
+};
